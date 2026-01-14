d71b1fb607973d449f316e1e32add4d2abeb17f5
Karol8284 <93425066+Karol8284@users.noreply.github.com>
2026-01-14 23:17:20 +0100

    Utworzenie poprawnej struktury 6 projektó :)

---
+++ BalzorPopups.Core.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace BalzorPopups.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ BalzorPopups.MudUI.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace BalzorPopups.MudUI.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
