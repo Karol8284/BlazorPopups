e2bab3d28b847517bbe8dabba58703b9f76b206d
Karol8284 <93425066+Karol8284@users.noreply.github.com>
2026-01-15 02:12:27 +0100

    Kod działą wyświetla się popup ale nie działą jeszcze opcja zamknięcia

--- BlazorPopups.Core.Demo/Program.cs
+++ BlazorPopups.Core.Demo/Program.cs
@@ -1,4 +1,6 @@
 using BlazorPopups.Core.Demo;
+using BlazorPopups.Core.Pages;
+using BlazorPopups.Core.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<PopupService>();
 
 await builder.Build().RunAsync();
---
+++ BlazorPopups.Core/Services/PopupService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPopups.Core.Services
+{
+    public class PopupService
+    {
+        public event Action<string>? OnShow;
+        public void Show(string messange)
+        {
+            OnShow?.Invoke(messange);
+        }
+    }
+}
